0727f784da93d75226674c57144d936e499d1197
Sergio romero <sergior500@gmail.com>
2022-12-02 10:16:37 +0100

    arreglo en el menu

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,10 @@
 let burger = document.getElementById('burger'),
 	 nav    = document.getElementById('main-nav');
+let container = document.getElementsByClassName('container');
 
 burger.addEventListener('click', function(e){
+
+	container[0].classList.toggle('is-open');
 	this.classList.toggle('is-open');
 	nav.classList.toggle('is-open');
 });
